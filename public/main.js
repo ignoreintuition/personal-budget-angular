95faab48190ea4ad1e3aa94ea542663717ded7d7
Brian Greig <brian@resurgencewebdesign.com>
2025-02-08 17:11:51 -0500

    Added D3JS chart

--- public/main.js
+++ public/main.js
@@ -1,38 +1,4 @@
-const ctx = document.getElementById("myChart");
-const colors = [
-  "#ea5545",
-  "#f46a9b",
-  "#ef9b20",
-  "#edbf33",
-  "#ede15b",
-  "#bdcf32",
-  "#87bc45",
-  "#27aeef",
-  "#b33dc6",
-];
-
-const getBudget = async () => {
-  const res = await axios.get("/budget");
-  return {
-    labels: res?.data?.myBudget.map((d) => d.title),
-    data: res?.data.myBudget.map((d) => d.budget),
-  };
-};
-
-const { labels, data } = await getBudget();
-const config = {
-  type: "doughnut",
-  data: {
-    labels,
-    datasets: [
-      {
-        data,
-        label: "Budget",
-        backgroundColor: colors,
-        hoverOffset: 4,
-      },
-    ],
-  },
-};
-
-new Chart(ctx, config);
+import drawChart from "/chart.js";
+import drawD3Chart from "/d3chart.js";
+await drawChart();
+await drawD3Chart();
